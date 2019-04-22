82e97c518c45f44c7da492a236beb8c80e5bdb79
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-04-22 16:00:20 -0300

    scrolling dragbles

--- client/src/app/shared/sketcher/sketcher.component.ts
+++ client/src/app/shared/sketcher/sketcher.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer
+} from "@angular/core";
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -10,9 +16,7 @@ import {
   CdkDragRelease,
   CdkDragStart
 } from "@angular/cdk/drag-drop";
-import {
-  OneImageData, TemplateSchemaData
-} from "./templates";
+import { OneImageData, TemplateSchemaData } from "./templates";
 
 @Component({
   selector: "app-sketcher",
@@ -20,26 +24,39 @@ import {
   styleUrls: ["./sketcher.component.scss"]
 })
 export class SketcherComponent implements OnInit {
+  @ViewChild("imageList") imagesList: ElementRef;
+  @ViewChild("buttonShiftTop") buttonTop: ElementRef;
+  @ViewChild("buttonShiftBottom") buttonBottom: ElementRef;
+
   private images = [];
   private contents: TemplateSchemaData[] = [];
   private templates: TemplateSchemaData[] = [];
 
+  private isShiftingTop = false;
+  private isShiftingBottom = false;
+
   private lastPostion;
 
-  constructor() {
+  private carouselCursor = 0;
+
+  private scrollValue = 0;
+  private scrollStep = 50;
+
+  private isDragging = false;
+
+  constructor(private renderer: Renderer) {
     this.fillPokemonList();
     this.fillTemplateList();
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
   fillPokemonList() {
     const baseURL =
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
     const sufixURL = ".png";
-    for (let i = 0; i < 10; i++) {
+    this.images = []
+    for (let i = 0; i < 6; i++) {
       const pokemon = Math.round(Math.random() * 600);
       this.images.push({
         src: `${baseURL}${pokemon}${sufixURL}`,
@@ -60,8 +77,14 @@ export class SketcherComponent implements OnInit {
     }
   }
 
-  ended(event: CdkDragEnd<{src:string, alt:string}>) {
-    console.log('ended', event)
+  ended(event: CdkDragEnd<{ src: string; alt: string }>) {
+    console.log("ended", event);
+    this.isDragging = false;
+    event.source.element.nativeElement.style.setProperty(
+      "transform",
+      "translate(0px,0px)"
+    );
+    event.source._dragRef._passiveTransform = {x: 0, y: 0}
   }
 
   predicateTemplate(item: CdkDrag<OneImageData>) {
@@ -77,48 +100,49 @@ export class SketcherComponent implements OnInit {
         event.currentIndex
       );
     } else {
-      console.log('drop', event.item.data.img.src);
-      const data = event.item.data
+      console.log("drop", event.item.data.img.src);
+      const data = event.item.data;
       //this.contents.splice(event.currentIndex, 0, new OneImageData(data.img, data.text))
-      this.contents = [new OneImageData(data.img, data.text)]
+      this.contents = [new OneImageData(data.img, data.text)];
     }
   }
 
   enter(event: CdkDragEnter<OneImageData>) {
-    console.log('enter', event.item.data);
-    const data = event.item.data
+    console.log("enter", event.item.data);
+    const data = event.item.data;
     //this.contents[0] = [new OneImageData(data.img, data.text)]
   }
 
   release(event: CdkDragRelease<OneImageData>) {
-    
-    const delta = this.getDisplacement(event)
-    
-    console.log('release', event, delta)
+    const delta = this.getDisplacement(event);
+
+    console.log("release", event, delta);
     this.lastPostion = null;
-    
   }
 
-  private getDisplacement(event): {x:number, y: number} {
-    const delta = {x:Infinity, y:Infinity};
+  private getDisplacement(event): { x: number; y: number } {
+    const delta = { x: Infinity, y: Infinity };
 
-    const style = event.source.element.nativeElement.attributes.getNamedItem("style")
+    const style = event.source.element.nativeElement.attributes.getNamedItem(
+      "style"
+    );
 
-    if(style) {
-      let startValueIndex = style.value.indexOf('(') + 1
-      let styleValue = style.textContent.substring(startValueIndex)
-      let endValueIndex = styleValue.indexOf('p')
-      delta.x = +styleValue.substring(0,endValueIndex)
-      startValueIndex =  styleValue.indexOf(',') + 1
-      styleValue = styleValue.substring(startValueIndex)
-      endValueIndex = styleValue.indexOf('p')
-      delta.y = +styleValue.substring(0, endValueIndex)
+    if (style) {
+      let startValueIndex = style.value.indexOf("(") + 1;
+      let styleValue = style.textContent.substring(startValueIndex);
+      let endValueIndex = styleValue.indexOf("p");
+      delta.x = +styleValue.substring(0, endValueIndex);
+      startValueIndex = styleValue.indexOf(",") + 1;
+      styleValue = styleValue.substring(startValueIndex);
+      endValueIndex = styleValue.indexOf("p");
+      delta.y = +styleValue.substring(0, endValueIndex);
     }
     return delta;
   }
 
   start(event: CdkDragStart<OneImageData>) {
-    console.log('start', event, this.lastPostion)
+    console.log("start", event, this.lastPostion);
+    this.isDragging = true;
   }
 
   moved(event: CdkDragMove<OneImageData>) {
@@ -127,19 +151,79 @@ export class SketcherComponent implements OnInit {
   }
 
   enterLeft(event: CdkDragEnter<OneImageData>) {
-    console.log('enter left', event.item.data);
-    const data = event.item.data
+    console.log("enter left", event.item.data);
+    const data = event.item.data;
     //this.contents[0] = [new OneImageData(data.img, data.text)]
   }
 
   enterRight(event: CdkDragEnter<OneImageData>) {
-    console.log('enter right', event.item.data);
-    const data = event.item.data
+    console.log("enter right", event.item.data);
+    const data = event.item.data;
     //this.contents[0] = [new OneImageData(data.img, data.text)]
   }
 
   exit(event: CdkDragExit<OneImageData>) {
-    console.log('exit', event.item.data)
-    this.contents = []
+    console.log("exit", event.item.data);
+    this.contents = [];
+  }
+
+  minCarouselCursor() {
+    return 0;
+  }
+
+  maxCarouselCursor() {
+    let max = 0;
+    const CAROUSEL_OUT_BOX = 2; //top and bottom
+    const listLen = this.imagesList.nativeElement.childElementCount - 1;
+    const validBoxes = listLen - CAROUSEL_OUT_BOX;
+    max = listLen - validBoxes;
+    return max >= 0 ? max : 0;
+  }
+
+  hasSpace(direction) {
+    if (this.imagesList.nativeElement.children) {
+      const listElement = this.imagesList.nativeElement
+      const itemHeight = this.imagesList.nativeElement.children[0];
+      const allElementsHeight = itemHeight.clientHeight * listElement.childElementCount
+      const listElementHeight = listElement.clientHeight
+
+      /* if (direction === "top") {
+        return this.scrollValue >= this.scrollStep;
+      } else if (direction === "bottom") {
+        return this.scrollValue + allElementsHeight <= listElementHeight - this.scrollStep;
+      } */
+
+      //console.log(this.scrollValue, allElementsHeight, listElementHeight, this.scrollStep)
+      //console.log(this.scrollValue + allElementsHeight, listElementHeight - this.scrollStep)
+
+      if (direction === "top") {
+        return allElementsHeight + this.scrollValue - this.scrollStep >= listElementHeight;
+      } else if (direction === "bottom") {
+        return this.scrollValue < 0;
+      }
+    }
+    return false;
+  }
+
+  scrollList(direction) {
+    if (direction.wheelDeltaY) {
+      direction = direction.wheelDeltaY > 0 ? "top" : "bottom";
+    }
+
+    if (this.hasSpace(direction)) {
+      const nativeList = this.imagesList.nativeElement;
+      this.scrollValue +=
+        direction === "top" ? -this.scrollStep : this.scrollStep;
+      for (let i = 0; i < nativeList.childElementCount; i++) {
+        const currentItem = nativeList.children[i];
+        console.log(currentItem)
+        //this.renderer.setElementClass(currentBoxObj, 'shift-bottom', true);
+        currentItem.style.setProperty(
+          "transform",
+          "translateY(" + this.scrollValue + "%)"
+        );
+      }
+      this.carouselCursor++;
+    }
   }
 }
