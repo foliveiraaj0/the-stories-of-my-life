a4b855994d8732fb2568725bd26e684c4d6f239b
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-06-16 13:46:53 +0100

    fixing imports

--- client/src/app/shared/sketcher/sketcher.module.ts
+++ client/src/app/shared/sketcher/sketcher.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SketcherComponent } from "./sketcher.component";
 import { DragDropModule } from "@angular/cdk/drag-drop";
-import { CustomScrollModule } from "../custom-scroll/custom-scroll.module";
 import { TemplatesModule } from "src/app/templates/templates.module";
 
 @NgModule({
   declarations: [SketcherComponent],
-  imports: [CommonModule, DragDropModule, CustomScrollModule, TemplatesModule],
+  imports: [CommonModule, DragDropModule, TemplatesModule],
   providers: [],
   exports: [SketcherComponent]
 })
