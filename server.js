d16776e4334fa40d94b7347ec1fc1d4016d5a922
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-08-27 11:14:29 +0100

    adjusting cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+var cors = require("cors");
 var bodyParser = require("body-parser");
 
 const app = express();
@@ -9,23 +9,21 @@ const userRouter = require("./src/routers/user");
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: true}))
 
-/* app.use(
+app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:9001",
     allowedHeaders: ["Content-Type", "Authorization"]
     // ,credentials: true
   })
-); */
+);
 
 /* app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", 'http://localhost:9001');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, Authorization');
   next();
 }); */
-
-
 
 app.use(userRouter);
 
