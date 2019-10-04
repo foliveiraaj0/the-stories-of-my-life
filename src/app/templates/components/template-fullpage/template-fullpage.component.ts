b60c69a181ce2aec4cf5a90cf068cc25b1c39c6d
Fernando Oliveira <f.oliveira.aj0@gmail.com>
2019-10-04 12:33:38 +0100

    fullpage template adjusts

--- src/app/templates/components/template-fullpage/template-fullpage.component.ts
+++ src/app/templates/components/template-fullpage/template-fullpage.component.ts
@@ -1,42 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { TemplateComponent } from '../../template-component';
+import { Component, OnInit } from "@angular/core";
+import { TemplateComponent } from "../../template-component";
 
 @Component({
-  templateUrl: './template-fullpage.component.html',
-  styleUrls: ['./template-fullpage.component.scss']
+  templateUrl: "./template-fullpage.component.html",
+  styleUrls: ["./template-fullpage.component.scss"]
 })
-export class TemplateFullpageComponent extends TemplateComponent implements OnInit {
-
+export class TemplateFullpageComponent extends TemplateComponent
+  implements OnInit {
   constructor() {
     super();
-   }
-
-  ngOnInit() {
   }
+
+  ngOnInit() {}
 
   protected createTemplateItems() {
     this.templateData.id = `${this.templateData.id}`;
-    if(this.templateData.image) {
+    if (this.templateData.image) {
       this.templateData.image = `./assets/users/${this.templateData.image}.jpg`;
     }
   }
 
   protected setComponentData(data, componentId) {
-    //console.log('setComponentData');
-    console.log(data);
-    //console.log(JSON.stringify(this.templateData));
     this.templateData.image = data.src;
   }
 
   getImage(): string {
-    if(this.templateData.image) {
+    if (this.templateData.image) {
       return this.templateData.image;
     }
     return "";
   }
 
   getBackground(): string {
-    return "url(./assets/backgrounds/frame12.png)";
+    return `url(${this.templateData.backgroundImage})`;
+  }
+
+  getBackgroundStyle(): string {
+    const frameChoosen = this.templateData.backgroundImage
+      .replace("./assets/backgrounds/", "")
+      .replace(".png", "");
+    return `content-item__img--${frameChoosen}`;
   }
 
   getImageId(): string {
